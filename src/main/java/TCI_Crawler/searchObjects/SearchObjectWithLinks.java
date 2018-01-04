35f513100314a55b59945bc3fb327cbb5072e8b2
Preslav Gerchev <preslavgerchevmail@gmail.com>
2018-01-04 17:45:49 +0100

    Added documentation for search objects.

--- src/main/java/TCI_Crawler/searchObjects/SearchObjectWithLinks.java
+++ src/main/java/TCI_Crawler/searchObjects/SearchObjectWithLinks.java
@@ -1,22 +1,44 @@
 package TCI_Crawler.searchObjects;
 
-import java.io.Serializable;
 import java.util.List;
 
-public class SearchObjectWithLinks implements Serializable{
+/**
+ * A class, that represents a retrieved search object and all links found on a single crawled
+ * web page.
+ */
+public class SearchObjectWithLinks {
 
+    /**
+     * Represents the found search object. Value can be equal to null if no object was found.
+     */
     private final SearchObjectBase retrievedObject;
+
+    /**
+     * Represents the retrieved links from the web page. Can be empty list if no links were found.
+     */
     private final List<String> retrievedLinks;
 
+    /**
+     * Initializes a new instance of the {@link SearchObjectWithLinks} class.
+     *
+     * @param retrievedObject Value for {@link SearchObjectWithLinks#retrievedObject}
+     * @param retrievedLinks  Value for {@link SearchObjectWithLinks#retrievedLinks}
+     */
     public SearchObjectWithLinks(SearchObjectBase retrievedObject, List<String> retrievedLinks) {
         this.retrievedObject = retrievedObject;
         this.retrievedLinks = retrievedLinks;
     }
 
+    /**
+     * @return Gets the retrieved links. List can be empty if no links were found.
+     */
     public List<String> getRetrievedLinks() {
         return retrievedLinks;
     }
 
+    /**
+     * @return Gets the retrieved object. Can be null if no object was found.
+     */
     public SearchObjectBase getRetrievedObject() {
         return retrievedObject;
     }
