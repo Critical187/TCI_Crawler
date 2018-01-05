d54bb278ead82157bbed1cb869e36d2ebfdb7998
chiel zelders <kids-papa@hotmail.com>
2018-01-05 01:22:56 +0100

    Commented the Node and Searcher so that it's logical how the tree is made

--- src/main/java/TCI_Crawler/treeStructure/Node.java
+++ src/main/java/TCI_Crawler/treeStructure/Node.java
@@ -6,15 +6,20 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Node<T extends Comparable> implements Comparable {
-    TreeSet<Node> neighbours;
+    //The children of the Node, if it has none than it's a leaf.
+    TreeSet<Node> children;
+    //this holds the information of the node
     private T data;
+    //when searching the tree you won't want to visit the same node twice
     private boolean visited;
+    //The parent of the node, if zero that means that this little fellow is the Root.
     private Node<T> parent;
+    //The Depth of the node. Root will be zero children of Root will be 1.
     private int weight = -1;
 
     public Node(T data) {
         this.data = data;
-        this.neighbours = new TreeSet<>();
+        this.children = new TreeSet<>();
     }
 
     public boolean isVisited() {
@@ -37,34 +42,35 @@ public class Node<T extends Comparable> implements Comparable {
         return data;
     }
 
-    public void removeNeighbour(Node unwantedNeighbourNode) {
-        this.neighbours.remove(unwantedNeighbourNode);
+    public void removeChild(Node unwantedChildNode) {
+        this.children.remove(unwantedChildNode);
     }
 
-    public void addNeighbours(Node neighbourNode) {
-        this.neighbours.add(neighbourNode);
+    public void addChild(Node childNode) {
+        this.children.add(childNode);
     }
 
-    public List getNeighbours() {
-        return neighbours.stream().collect(Collectors.toCollection(ArrayList::new));
+    public List getChildren() {
+        return children.stream().collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public void setNeighbours(List neighbours) {
-        this.neighbours = new TreeSet<Node>(neighbours);
+    public void setChildren(List children) {
+        this.children = new TreeSet<Node>(children);
 
     }
 
     public int getWeight() {
         return weight;
     }
-
+    //it's pretty dynamic. The moment you hang the node higher in the tree the children automaticly get a smaller weight as well.
     public void setWeight(int weight) {
         this.weight = weight;
-        for (Node node : neighbours) {
+        for (Node node : children) {
             node.setWeight(weight);
         }
     }
-
+    //this bad boy checks if the data is the same
+    //if data is missing: it checks if the nodes are the same using the hashcodes.
     @Override
     public int compareTo(Object o) {
         int value = 0;
